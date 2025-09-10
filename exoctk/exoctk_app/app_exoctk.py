893bc6fd086fe04f02299ee6abacd8e7b4fe6616
york@stsci.edu <york@stsci.edu>
2025-09-10 09:21:32 -0400

    Running containerized celery

--- exoctk/exoctk_app/app_exoctk.py
+++ exoctk/exoctk_app/app_exoctk.py
@@ -42,8 +42,8 @@ app_exoctk.config['CACHE_TYPE'] = 'null'
 app_exoctk.config['SECRET_KEY'] = 'Thisisasecret!'
 
 # Configure Celery
-app_exoctk.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app_exoctk.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app_exoctk.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app_exoctk.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
 
 # Initialize Celery
 celery = Celery(app_exoctk.import_name, broker=app_exoctk.config['CELERY_BROKER_URL'],
